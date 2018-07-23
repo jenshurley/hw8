dc4dcc443e3e980a3258eb94d64dfbc8bcd1fe7a
Jennifer Shurley <jenshurley@gmail.com>
2018-07-23 17:40:37 -0600

    added answers for questions 1 and 2

--- film_queries.sql
+++ film_queries.sql
@@ -1,3 +1,31 @@
 -- hw8 film queries
---1a. Display the first and last names of all actors from the table actor.
 ...
+--1a 
+SELECT first_name, last_name 
+FROM actor;
+
+--1b
+SELECT UPPER(CONCAT(first_name, last_name)) as Actor_Name 
+FROM actor;
+
+--2a
+SELECT actor_id, first_name, last_name 
+FROM actor 
+WHERE first_name 
+LIKE 'Joe';
+
+--2b
+SELECT first_name, last_name 
+FROM actor 
+WHERE last_name 
+LIKE '%GEN%';
+
+--2c
+SELECT first_name, last_name 
+FROM actor 
+WHERE last_name LIKE '%LI'
+ORDER BY last_name, first_name;
+
+--2d
+SELECT country_id, country FROM country
+WHERE country IN ("Afghanistan", "Bangladesh", "China");
