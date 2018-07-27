b98233c63768dd0cf191a8f6aee249429e01aca2
Jennifer Shurley <jenshurley@gmail.com>
2018-07-27 10:51:55 -0600

    added 3 and 4

--- film_queries.sql
+++ film_queries.sql
@@ -29,3 +29,35 @@ ORDER BY last_name, first_name;
 --2d
 SELECT country_id, country FROM country
 WHERE country IN ("Afghanistan", "Bangladesh", "China");
+
+--3a
+ALTER TABLE actor
+ADD COLUMN middle_name VARCHAR(45) AFTER first_name;
+
+--3b
+ALTER TABLE actor
+MODIFY COLUMN middle_name blob;
+
+--3b
+ALTER TABLE actor
+DROP COLUMN middle_name;
+
+--4a
+SELECT last_name FROM actor;
+
+--4b
+SELECT last_name, COUNT(*) FROM actor 
+GROUP BY last_name;
+
+--4c
+ SELECT last_name, COUNT(*) as count FROM actor 
+ GROUP BY last_name 
+ HAVING count > 1;
+
+ --4d
+SELECT * FROM actor 
+WHERE last_name LIKE 'Williams';
+
+UPDATE actor
+SET first_name = 'HARPO'
+WHERE actor_id = 172;
